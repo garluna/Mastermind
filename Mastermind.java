43f639b29b2ea0e5b25e192a4dc9db4c4ba1cd50
Garima Lunawat <g.me.luna@gmail.com>
2022-12-26 20:18:54 -0800

    Create answer
    Computer generates random array of the 6 colors represented as characters

--- Mastermind.java
+++ Mastermind.java
@@ -1,8 +1,28 @@
+import java.util.Random;
+
 public class Mastermind {
 
+    public static char[] answer;
     
     public static void main(String[] args) {
+        // Create computer random answer
+        setAnswerSequence();
+
+        // Take guesses from user
+        // Tell how many are correct
+        // Show win when correct guess occurs
+        // Show lose if cant guess in 10 guesses
         
+    }
+
+    private static void setAnswerSequence() {
+        answer = new char[6];
+        char[] colors = {'R', 'G', 'B', 'Y', 'O', 'P'};
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            int x = random.nextInt(6);
+            answer[i] = colors[x];
+        }
     }
 
 }
